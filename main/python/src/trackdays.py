65e0a922249dfcf9967a6471d6218a2c1507e76f
gmelson <gmelson@yahoo.com>
2013-09-04 09:53:01 -0700

    moved source to new directory

--- main/python/src/trackdays.py
+++ main/python/src/trackdays.py
@@ -1,71 +1,38 @@
 import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import urlfetch
 
-from proxy import Proxy
-from schedule import Schedule
+from trackday import Trackday
 
 class UpdatePage(webapp.RequestHandler):
-                
-    def get(self):
-        proxy = None
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Update successful!')
-        json = self.request.get('json')
-        logging.info('full url of proxied server ' + self.request.url)
-        ip = self.request.remote_addr
-        p = Proxy.query()
-        for _p in p:
-            if _p.proxiedip == ip:
-                proxy = _p
-                break            
-        if not proxy:
-            proxy = Proxy(proxiedip = ip)
-            proxy.put()
-
-class PushData(webapp.RequestHandler):
+                           
     def post(self):
-        #jsondata = self.request.get('jsondata')
-        s = Schedule.query()
+        s = Trackday.query()
         for _s in s:
-            if _s.name == 'GAM':
-                schedule = _s
-                break            
-        if not schedule:
-            schedule = Schedule(name = 'GAM')
-        schedule.jsondata = jsondata    
-        schedule.put()
+            _s.key.delete()
+        trackday = Trackday(jsondata = self.request.body)
+        lastupdated = datetime.datetime.now()
+        trackday.put()
         
 
 class MainPage(webapp.RequestHandler):
     
-    def getProxiedTracks(self):
-        proxies = Proxy.query()
-        for proxy in proxies: 
-            try:
-                url = 'http://' + proxy.proxiedip + ':8180/gettracks'
-                logging.info('url to fetch ' + url)
-                result = urlfetch.fetch(url)
-                logging.info('result from proxied server ' + result)
-                self.response.out.write(result.content)        
-            except Exception:
-                logging.info('error trying to request ' + url)
-                continue
-        
                 
     def get(self):
+
+        clientupdated = self.request.get("lastupdated")
         self.response.headers['Content-Type'] = 'application/json'
-        schedules = Schedule.query()
-        for schedule in schedules :
-            try:
-                self.response.out.write(schedule.jsondata)
-            except Exception:
-                logging.info('error getting schedule from db')
-                continue
+        try:
+            trackdays = Trackday.query()
+            for trackday in trackdays:
+                self.response.out.write(trackday.lastupdated)
+                if(trackday.lastupdated > clientupdated) :
+                    self.response.out.write(trackday.jsondata)
+        except Exception:
+            logging.info('error getting trackdays from db')
 
 
-application = webapp.WSGIApplication([('/', MainPage),('/update', UpdatePage), ('/push'), PushData], debug=True)
+application = webapp.WSGIApplication([('/', MainPage),('/update', UpdatePage)], debug=True)
 
 
 def main():
