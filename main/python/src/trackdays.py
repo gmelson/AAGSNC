f48b5dd8f9b69b37d95ba9a40687267b448b65a1
gmelson <gmelson@yahoo.com>
2013-09-12 17:23:38 -0700

    Correct bugs with lastupdated versus lastUpdated; Set lastUpdated correctly with update

--- main/python/src/trackdays.py
+++ main/python/src/trackdays.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import datetime
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -11,26 +12,32 @@ class UpdatePage(webapp.RequestHandler):
         s = Trackday.query()
         for _s in s:
             _s.key.delete()
-        trackday = Trackday(jsondata = self.request.body)
-        lastupdated = datetime.datetime.now()
+
+        trackday = Trackday(jsondata = self.request.body, lastUpdated=datetime.datetime.now())        
         trackday.put()
         
 
 class MainPage(webapp.RequestHandler):
     
-                
+    def writeData(self, data):
+        self.response.out.write(data)
+        
     def get(self):
-
         clientupdated = self.request.get("lastupdated")
         self.response.headers['Content-Type'] = 'application/json'
         try:
             trackdays = Trackday.query()
             for trackday in trackdays:
-                self.response.out.write(trackday.lastupdated)
-                if(trackday.lastupdated > clientupdated) :
-                    self.response.out.write(trackday.jsondata)
+                if trackday.lastUpdated:
+                    self.response.out.write(trackday.lastUpdated)
+                    if(trackday.lastUpdated > clientupdated) :
+                        self.writeData(trackday.jsondata)
+                        #self.response.out.write(trackday.jsondata)
+                else:
+                    self.writeData(trackday.jsondata)
         except Exception:
-            logging.info('error getting trackdays from db')
+            e = sys.exc_info()[0]
+            logging.info('error getting trackdays from db %s' % str(e))
 
 
 application = webapp.WSGIApplication([('/', MainPage),('/update', UpdatePage)], debug=True)
